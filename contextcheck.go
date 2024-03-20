87e3db6fc6679c08b1365dcf8b1c5bd33972cdfb
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-03-20 18:09:05 +0100

    chore: add GitHub Action

--- contextcheck.go
+++ contextcheck.go
@@ -4,7 +4,6 @@ import (
 	"go/ast"
 	"go/types"
 	"regexp"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -214,37 +213,6 @@ func (r *runner) collectHttpTyps(pssa *buildssa.SSA) {
 	if ok {
 		r.httpReqTyps = append(r.httpReqTyps, pobjReq)
 	}
-}
-
-func (r *runner) noImportedContextAndHttp(f *ssa.Function) (ret bool) {
-	if !f.Pos().IsValid() {
-		return false
-	}
-
-	file := analysisutil.File(r.pass, f.Pos())
-	if file == nil {
-		return false
-	}
-
-	if skip, has := r.skipFile[file]; has {
-		return skip
-	}
-	defer func() {
-		r.skipFile[file] = ret
-	}()
-
-	for _, impt := range file.Imports {
-		path, err := strconv.Unquote(impt.Path.Value)
-		if err != nil {
-			continue
-		}
-		path = analysisutil.RemoveVendor(path)
-		if path == ctxPkg || path == httpPkg {
-			return false
-		}
-	}
-
-	return true
 }
 
 func (r *runner) checkIsEntry(f *ssa.Function) (ret entryType) {
